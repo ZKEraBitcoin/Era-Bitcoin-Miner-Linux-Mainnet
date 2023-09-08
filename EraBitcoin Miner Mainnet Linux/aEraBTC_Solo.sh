1e142800c5a08e7f5215dffcab0b1006cc8dba53
ZKEraBitcoin <142457573+ZKEraBitcoin@users.noreply.github.com>
2023-09-07 21:35:56 -0500

    Update aEraBTC_Solo.sh

--- EraBitcoin Miner Mainnet Linux/aEraBTC_Solo.sh
+++ EraBitcoin Miner Mainnet Linux/aEraBTC_Solo.sh
@@ -11,6 +11,6 @@ while : ; do
   if [ -f EraBitcoinMiner.conf ] ; then
     rm -f EraBitcoinMiner.conf
   fi
-  dotnet EraBitcoinMiner.dll allowCPU=false allowIntel=true allowAMD=true allowCUDA=true web3api=https://zksync2-testnet.zksync.dev abiFile=EraBTC.abi contract=0x6dCB84f24c0e5B912e2DbC8E65FA4f1Ac3985232 gasToMine=1 gasApiMax=1 gasLimit=3704624 gasApiURL= gasApiPath=$.safeLow gasApiMultiplier=0.1 gasApiOffset=1.0 minutesBetweenMints=1.0 BlocksPerMint=1.0 privateKey=33185a4f45eb65419fcc5dc0ade6c97c1fcf93212df1de53985b74e272805bcb
+  dotnet EraBitcoinMiner.dll allowCPU=false allowIntel=true allowAMD=true allowCUDA=true web3api=https://mainnet.era.zksync.io abiFile=EraBTC.abi contract=0xb3f7a1f441bf0B5Be50D800cF2ff1dEE91dB7c61 gasToMine=0.25 gasApiMax=1 gasLimit=3704624 gasApiURL= gasApiPath=$.safeLow gasApiMultiplier=0.1 gasApiOffset=0.25 minutesBetweenMints=5.0 BlocksPerMint=1.0 privateKey=33185a4f45eb65419fcc5dc0ade6c97c1fcf93212df1de53985b74e272805bcb
   [[ $? -eq 22 ]] || break
 done
